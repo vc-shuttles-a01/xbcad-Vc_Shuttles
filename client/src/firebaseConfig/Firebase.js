39e05f5b20ffdea16375555c88de805b51011299
mahmoud1903 <107139934+mahmoud1903@users.noreply.github.com>
2024-11-24 14:51:07 +0200

    Update Firebase.js

--- client/src/firebaseConfig/Firebase.js
+++ client/src/firebaseConfig/Firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3dCEu8Y2qRE22ve0fq88JkEW97b_q3h4",
-  authDomain: "shuttleapp-75184.firebaseapp.com",
-  projectId: "shuttleapp-75184",
-  storageBucket: "shuttleapp-75184.firebasestorage.app",
-  messagingSenderId: "386782838163",
-  appId: "1:386782838163:web:67454b6d44d972f5953c66",
-  measurementId: "G-13KNP4214Q"
+ 
 };
 const app = initializeApp(firebaseConfig);
 
